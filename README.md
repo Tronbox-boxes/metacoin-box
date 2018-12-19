1b92c3ee9ab9ea9a5fdfd276ab973f1eaa4cc2a0
Francesco Sullo <sullof@gmail.com>
2018-12-18 23:43:24 -0800

    More clear README

--- README.md
+++ README.md
@@ -12,32 +12,20 @@ module.exports = {
 
 // trontools/quickstart docker image
     development: {
-      privateKey: // don't change this private key
-      'da146374a75310b9666e834ee4ad0866d6f4035967bfc76217c5a495fff9f0d0',
-      fullHost: "http://127.0.0.1:9090",
-      feeLimit: 1e8,
-      userFeePercentage: 30,
-      originEnergyLimit: 1e5,
-      network_id: "*"
-    },
-
-// TronGrid Shasta testnet
-    shasta: {
-
-      // Put your shasta account's private key here:
-      privateKey: '',
-
-      // To receive trx for your shasta account go to
-      // https://www.trongrid.io/shasta
-      // and paste your address in the field at the bottom of the page
-
-      consume_user_resource_percent: 30,
-      fullHost: "https://api.shasta.trongrid.io",
-      feeLimit: 1e8,
-      userFeePercentage: 30,
-      originEnergyLimit: 1e5,
-      network_id: "*"
-    }
+          // For trontools/quickstart docker image
+          privateKey: 'da146374a75310b9666e834ee4ad0866d6f4035967bfc76217c5a495fff9f0d0',
+          consume_user_resource_percent: 30,
+          fee_limit: 100000000,
+          fullHost: "http://127.0.0.1:9090",
+          network_id: "9090"
+        },
+        shasta: {
+          privateKey: process.env.PK,
+          consume_user_resource_percent: 30,
+          fee_limit: 100000000,
+          fullHost: "https://api.shasta.trongrid.io",
+          network_id: "2"
+        }
   }
 }
 ```
--- tronbox.js
+++ tronbox.js
@@ -5,16 +5,15 @@ module.exports = {
       privateKey: 'da146374a75310b9666e834ee4ad0866d6f4035967bfc76217c5a495fff9f0d0',
       consume_user_resource_percent: 30,
       fee_limit: 100000000,
-
-      // Requires TronBox 2.1.9+ and Tron Quickstart 1.1.16+
-      // fullHost: "http://127.0.0.1:9090",
-
-      // The three settings below for TronBox < 2.1.9
-      fullNode: "http://127.0.0.1:9090",
-      solidityNode: "http://127.0.0.1:9090",
-      eventServer: "http://127.0.0.1:9090",
-
+      fullHost: "http://127.0.0.1:9090",
       network_id: "9090"
+    },
+    shasta: {
+      privateKey: process.env.PK,
+      consume_user_resource_percent: 30,
+      fee_limit: 100000000,
+      fullHost: "https://api.shasta.trongrid.io",
+      network_id: "2"
     },
     mainnet: {
       // Don't put your private key here:
@@ -31,31 +30,8 @@ Then, run the migration with:
 */
       consume_user_resource_percent: 30,
       fee_limit: 100000000,
-
-      // tronbox 2.1.9+
-      // fullHost: "https://api.trongrid.io",
-
-      // tronbox < 2.1.9
-      fullNode: "https://api.trongrid.io",
-      solidityNode: "https://api.trongrid.io",
-      eventServer: "https://api.trongrid.io",
-
+      fullHost: "https://api.trongrid.io",
       network_id: "1"
-    },
-    shasta: {
-      privateKey: process.env.PK,
-      consume_user_resource_percent: 30,
-      fee_limit: 100000000,
-
-      // tronbox 2.1.9+
-      // fullHost: "https://api.shasta.trongrid.io",
-
-      // tronbox < 2.1.9
-      fullNode: "https://api.shasta.trongrid.io",
-      solidityNode: "https://api.shasta.trongrid.io",
-      eventServer: "https://api.shasta.trongrid.io",
-
-      network_id: "2"
     }
   }
 }
