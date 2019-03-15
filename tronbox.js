6d50b18b9fd1066efd795b48a9074d80cf2e9964
Francesco Sullo <sullof@gmail.com>
2019-03-15 13:03:21 -0700

    Fix tronbox.js 3

--- tronbox.js
+++ tronbox.js
@@ -2,21 +2,6 @@ const port = process.env.HOST_PORT || 9090
 
 module.exports = {
   networks: {
-    development: {
-      // For trontools/quickstart docker image
-      privateKey: 'da146374a75310b9666e834ee4ad0866d6f4035967bfc76217c5a495fff9f0d0',
-      userFeePercentage: 0,
-      feeLimit: 1e8,
-      fullHost: 'http://127.0.0.1:' + port,
-      network_id: "9"
-    },
-    shasta: {
-      privateKey: process.env.PRIVATE_KEY_SHASTA,
-      userFeePercentage: 50,
-      feeLimit: 1e8,
-      fullHost: "https://api.shasta.trongrid.io",
-      network_id: "2"
-    },
     mainnet: {
       // Don't put your private key here:
       privateKey: process.env.PRIVATE_KEY_MAINNET,
@@ -34,6 +19,21 @@ Then, run the migration with:
       feeLimit: 1e8,
       fullHost: "https://api.trongrid.io",
       network_id: "1"
+    },
+    shasta: {
+      privateKey: process.env.PRIVATE_KEY_SHASTA,
+      userFeePercentage: 50,
+      feeLimit: 1e8,
+      fullHost: "https://api.shasta.trongrid.io",
+      network_id: "2"
+    },
+    development: {
+      // For trontools/quickstart docker image
+      privateKey: 'da146374a75310b9666e834ee4ad0866d6f4035967bfc76217c5a495fff9f0d0',
+      userFeePercentage: 0,
+      feeLimit: 1e8,
+      fullHost: 'http://127.0.0.1:' + port,
+      network_id: "9"
     }
   }
 }
